47cc8806329a9c14e0cb47802be8de728d64ad80
ebrusoni <ebrusoni@student.ethz.ch>
2025-05-04 11:56:54 +0200

    add data_path selection code for loss_analysis, to differentiate between renkolab and local

--- src/fco2models/ueval.py
+++ src/fco2models/ueval.py
@@ -2,6 +2,37 @@ import json
 import torch
 from diffusers import DDPMScheduler
 
+def add_src_and_logger(is_renkolab, save_dir):
+    import sys
+    from pathlib import Path
+    import os
+    import logging 
+    src_path = Path.home() / "work" / "fco2diffusion" / "src"
+    if str(src_path) not in sys.path:
+        sys.path.insert(0, str(src_path))
+
+    if is_renkolab:
+        DATA_PATH = '/home/jovyan/work/datapolybox/'
+    else:
+        DATA_PATH = '../data/training_data/'
+    
+    if (save_dir is not None) and (not os.path.exists(save_dir)):
+        os.makedirs(save_dir)
+
+    if save_dir is None:
+        logging.basicConfig(
+            level=logging.INFO, 
+            format='%(asctime)s - %(levelname)s - %(message)s'
+            )
+    else:
+        logging.basicConfig(
+            filename=save_dir+'training.log',
+            filemode='a',
+            level=logging.INFO, 
+            format='%(asctime)s - %(levelname)s - %(message)s'
+            )
+    return DATA_PATH, logging
+
 def load_models(models_info):
     models = dict()
     for model_name, info in models_info.items():
