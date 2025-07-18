698d56f4752ffb9f91fc1ae1b80425d14f3b67b1
ebrusoni <ebrusoni@student.ethz.ch>
2025-07-18 15:35:56 +0200

    script for predicting val set with mlp

---
+++ euler/mlp_expo.py
@@ -0,0 +1,80 @@
+from utils import add_src_and_logger
+save_dir = f'../models/anoms_sea/'
+is_renkulab = True
+DATA_PATH, logging = add_src_and_logger(is_renkulab, None)
+
+import pandas as pd
+import numpy as np
+import torch
+from tqdm import tqdm
+from torch.utils.data import TensorDataset, DataLoader
+from fco2models.models import MLPNaiveEnsemble
+from fco2models.ueval import load_models
+from fco2models.utraining import prep_df, make_monthly_split
+
+model_info = {
+    'sota_ensemble': ['../models/sota_ensemble/', 'e_30.pt', MLPNaiveEnsemble],
+}
+models = load_models(model_info)
+
+
+DATA_PATH = "../data/training_data/"
+df = pd.read_parquet(DATA_PATH + "SOCAT_1982_2021_grouped_colloc_augm_bin.pq", engine='pyarrow')
+df = prep_df(df, bound=True, add_clim=False, add_seas=False)[0]
+_, mask_val, mask_test = make_monthly_split(df)
+df_val = df.loc[df.expocode.map(mask_val), :]
+df_test = df.loc[df.expocode.map(mask_test), :]
+
+def normalize(df, stats, mode):
+    for i in range(1, len(df.columns)): # first column is the target
+        col = df.columns[i - 1]
+        # print(f"Normalizing {col} with {mode}")
+        if mode == 'min_max':
+            # print(f"Min: {stats['mins'][i]}, Max: {stats['maxs'][i]}")
+            df[col] = 2 * (df[col] - stats['mins'][i]) / (stats['maxs'][i] - stats['mins'][i]) - 1
+        elif mode == 'mean_std':
+            df[col] = (df[col] - stats['means'][i]) / stats['stds'][i]
+        else:
+            raise ValueError(f"Unknown mode {mode}")
+    return df
+
+def get_samples_ensemble(df, model_info):
+    params = model_info['params']
+    predictors = params['predictors']
+    stats = {
+        'means': params['train_means'],
+        'stds': params['train_stds'],
+        'mins': params['train_mins'],
+        'maxs': params['train_maxs'],
+    }
+    df.loc[:, predictors] = normalize(df.loc[:, predictors], stats, params['mode'])
+
+    model = model_info['model']
+    ds = torch.from_numpy(df.loc[:, predictors].values).float()
+    print(ds.shape)
+    ds = TensorDataset(ds.unsqueeze(-1))
+    dataloader = DataLoader(ds, batch_size=128, shuffle=False)
+    samples = []
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    model.to(device)
+
+    # _, samples = predict_mean_eval(model, dataloader)
+    for batch in tqdm(dataloader, desc="Ensemble sampling"):
+        batch = batch[0].to(device)
+        with torch.no_grad():
+            sample = model(batch, 0, return_dict=False)[0]
+        samples.append(sample.cpu().numpy())
+    samples = np.concatenate(samples, axis=1)
+
+    print(f"Samples shape: {samples.shape}")
+    return samples 
+
+from fco2models.ueval import rescale
+samples = get_samples_ensemble(df_val.copy(), models['sota_ensemble'])
+params = models['sota_ensemble']['params']
+samples = rescale(samples.reshape(-1, 1), params, params['mode']).reshape(20, -1)
+
+mlp_pred_cols = [f'mlp_{i}' for i in range(samples.shape[0])]
+df_val.loc[:, mlp_pred_cols] = samples.T
+
+df_val.to_parquet(f'{save_dir}val_predictions.pq')
