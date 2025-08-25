5cb5118fea46c4f449dd905444f07e644bf2d57d
ebrusoni <ebrusoni@student.ethz.ch>
2025-08-25 17:25:46 +0200

    add expocode selection and comments for clarity

--- euler/expo.py
+++ euler/expo.py
@@ -34,7 +34,7 @@ def diffusion_step(model, noise_scheduler, x, t, jump):
             sample = noise_scheduler.add_noise(x_0, torch.randn_like(sample), t - jump)
     else:
         # Update sample with step
-        sample = output_scheduler.prev_sample
+        sample = output_scheduler.prev_sample # this is used with the DDIM sampler
     return sample
 
 def step_and_collocate(model, t, dataloader, noise_scheduler, n_recs, expo_ids, windows, recs):
@@ -65,9 +65,9 @@ def init_denoise_df(df, n_recs=10, n=64):
     Initialize a DataFrame for denoising with the required columns.
     """
     rec_cols = [f'rec_{i}' for i in range(n_recs)]
-    df = reflect_dataframe_edges(df, n=n)
+    df = reflect_dataframe_edges(df, n=n) # pad edges of each cruise
     df.loc[:, rec_cols] = np.random.randn(len(df), n_recs)  # Random values for recs
-    df.loc[:, 'expocode_id'] = df.loc[:, 'expocode'].map(get_expocode_map(df.loc[:, :]))
+    df.loc[:, 'expocode_id'] = df.loc[:, 'expocode'].map(get_expocode_map(df.loc[:, :])) # map expocode to int id for ndarray bookkeeping
     return df
 
 def reflect_dataframe_edges(df: pd.DataFrame, group_col: str = "expocode", n: int = 64) -> pd.DataFrame:
@@ -84,26 +84,18 @@ def reflect_dataframe_edges(df: pd.DataFrame, group_col: str = "expocode", n: in
     
     def repeat_group(group):
         g_n = len(group)
-        #if g_n < n:
-        #    rest = n - g_n
-        #    return group
         
         # Repeat top n rows m times
         top_repeat = pd.concat([group.iloc[[0]]] * n, axis=0)
-        #print(top_repeat)
         top_repeat.window_id = np.arange(-n, 0, 1)  # Assign window IDs (adjust logic if needed)
 
         # Repeat bottom n rows m times
-        rest = n - g_n % n if g_n >= n else n - g_n
+        rest = n - g_n % n if g_n >= n else n - g_n # add also a small rest to make track sizes divisible by 64
         bottom_repeat = pd.concat([group.iloc[[-1]]] * (n + rest), axis=0) # extend to n exactly so that get_segments_fast works
         bottom_repeat.window_id = np.arange(g_n, g_n + n + rest, 1)
 
         #Concatenate top repeats, original group, bottom repeats
         return pd.concat([top_repeat, group, bottom_repeat], ignore_index=True)
-    
-    #padded = df.groupby(group_col, group_keys=False).apply(repeat_group).reset_index(drop=True)
-    #print(padded.head(64))
-    #print(padded.tail(64))
     
     return df.groupby(group_col, group_keys=False)[df.columns].apply(repeat_group).reset_index(drop=True)
 
@@ -117,7 +109,9 @@ def get_segments_fast(df, cols, num_windows=64, step=64, offset=0):
     # windows: (N - num_windows + 1, C, num_windows)
     windows = sliding_window_view(arr, num_windows, axis=0)
     windows = windows[offset::step]                           # stride in one go
-    return windows
+    # Note that windows of neighbouring cruise tracks (which are completely unrelated) can have overlapping paddings.
+    # This is not optimal, but it is much easier and faster to implement.
+    return windows 
 
 def step_get_samples(model, t, dataloader, noise_scheduler):
     outs = []
@@ -132,15 +126,16 @@ def step_get_samples(model, t, dataloader, noise_scheduler):
 def segment_rec_df(df, predictors, n_recs, segment_len=64):
     """
     Segment the DataFrame into smaller chunks for processing.
+    Include data as well as indexing information (expocode_id, window_id, rec_id)
     """
     segments = []
     cols =  predictors + ['expocode_id', 'window_id']
     random_offset = np.random.randint(0, 64, size=n_recs)
     for i in range(n_recs):
         rec = f'rec_{i}'
-        segments_rec = get_segments_fast(df, [rec] + cols, offset=random_offset[i])
-        rec_col = np.full_like(segments_rec[:, 0:1, :], i)
-        segments_rec = np.concatenate([segments_rec, rec_col], axis=1)
+        segments_rec = get_segments_fast(df, [rec] + cols, offset=random_offset[i]) # get segments for every sample with a different offset
+        rec_col = np.full_like(segments_rec[:, 0:1, :], i) # column storing sample id for assigning to correct column after denoising step
+        segments_rec = np.concatenate([segments_rec, rec_col], axis=1) # concat data and sample id column (each datapoint is now indexed by: expocode_id, window_id, rec_id)
         segments.append(segments_rec)
 
     segments = np.concatenate(segments, axis=0)
@@ -159,12 +154,11 @@ def denoise_df(df, model_info, n_recs, jump=20):
         num_train_timesteps=noise_scheduler.config.num_train_timesteps,
         beta_schedule=noise_scheduler.config.beta_schedule,
         clip_sample_range=noise_scheduler.config.clip_sample_range,
-        #timestep_spacing="trailing"
        )
-    timesteps = np.concatenate((np.arange(0, 40, 2), np.arange(40, 1000, 20)))[::-1]
+    #timesteps = np.concatenate((np.arange(0, 40, 2), np.arange(40, 1000, 20)))[::-1]
     #print(timesteps)
     # /opt/conda/lib/python3.10/site-packages/diffusers/schedulers/scheduling_ddim.py, line 335 for passing inference steps as list
-    ddim_scheduler.set_timesteps(50, steps=None)
+    ddim_scheduler.set_timesteps(50)
    
     stats = {
         'means': params['train_means'],
@@ -202,6 +196,11 @@ def denoise_df(df, model_info, n_recs, jump=20):
     return denoise_df
 
 def denoise_df_2(df, model_info, n_recs, jump=None, n=64):
+    """
+    Generate smooth track samples using shifting procedure outlines in Section 3.3 of the writeup
+    This version was made mostly with chatGPT to move all data processing to numpy arrays.
+    Denoise_df was made by me and works with dataframes (easier to index, but much slower)
+    """
     params = model_info['params']
     predictors = params['predictors']
     model = model_info['model']
@@ -212,8 +211,7 @@ def denoise_df_2(df, model_info, n_recs, jump=None, n=64):
         beta_schedule=noise_scheduler.config.beta_schedule,
         clip_sample_range=noise_scheduler.config.clip_sample_range,
     )
-    timesteps = np.concatenate((np.arange(0, 40, 2), np.arange(40, 1000, 20)))[::-1]
-    ddim_scheduler.set_timesteps(50, steps=None)
+    ddim_scheduler.set_timesteps(50) # 50 equally spaced time steps 
 
     stats = {
         'means': params['train_means'],
@@ -223,7 +221,7 @@ def denoise_df_2(df, model_info, n_recs, jump=None, n=64):
     }
 
     df.loc[:, predictors] = normalize(df.loc[:, predictors], stats, params['mode'])
-    denoise_df = init_denoise_df(df, n_recs=n_recs, n=n)
+    denoise_df = init_denoise_df(df, n_recs=n_recs, n=n) # initialize dataframe for procedure
     rec_cols = [f'rec_{i}' for i in range(n_recs)]
 
     # Ensure integer dtypes for keys (helps get_indexer)
@@ -240,31 +238,33 @@ def denoise_df_2(df, model_info, n_recs, jump=None, n=64):
     model.to(device)
 
     for t in t_loop:
-        ds = segment_rec_df(denoise_df, predictors, n_recs)
+        ds = segment_rec_df(denoise_df, predictors, n_recs) # split cruise in segments and include indexing information
 
+        # indexing info
         expo_ids = ds[:, -3, :].astype(np.int64)  # (N, L)
         windows  = ds[:, -2, :].astype(np.int64)  # (N, L)
         recs     = ds[:, -1, :].astype(np.int64)  # (N, L)
 
+        # input to the model data (remoce indexing columns) and add all-one mask
         model_input = np.concatenate([ds[:, :-3, :], np.ones_like(ds[:, 0:1, :])], axis=1)
         model_input = torch.from_numpy(model_input).float()       # stay on CPU here
         dataloader = DataLoader(model_input, batch_size=2048, shuffle=False, pin_memory=True)
 
+        # get denoised samples using the model
         samples = step_get_samples(model, t, dataloader, ddim_scheduler)  # (N, L)
 
         # Vectorised row lookup: -1 where (expocode_id, window_id) is missing
         idx = pd.MultiIndex.from_arrays([expo_ids.ravel(), windows.ravel()])
         rows = row_mi.get_indexer(idx)                      # ndarray of int64 (−1 for miss)
         mask = rows != -1
-        #print(mask.sum())
-        #print(mask.shape)
         if mask.any():
+            # use indexing information
             rows_i = rows[mask]
             cols_i = recs.ravel()[mask]
             vals   = samples.ravel()[mask]
             rec_block[rows_i, cols_i] = vals
         
-        denoise_df[rec_cols] = rec_block
+        denoise_df[rec_cols] = rec_block # save reconstruction in df for the next iteration
     return denoise_df
 
 
@@ -295,32 +295,32 @@ df_train = df.loc[df.expocode.map(mask_train), :]
 df_val = df.loc[df.expocode.map(mask_val), :]
 df_test = df.loc[df.expocode.map(mask_test), :]
 
-expocodes = ["AG5W20141113"]
+expocodes = ["AG5W20141113", "33RO20180307", "49P120060717", "49P120100104", "AG5W20141113", "AG5W20151120", "AG5W20170115"]
 #expocodes = df_val.expocode.unique()
 n_recs = 50
 rec_cols = [f'rec_{i}' for i in range(n_recs)]
 model_info = models['dm']
-#model_info['model'].set_w(0.5)
+#model_info['model'].set_w(0.5) # set classifier-free guidance weight
 params = model_info['params']
 #df_val = impute_df(df_val, params['predictors'])
-df_test = impute_df(df_test, params['predictors'])
+df_test = impute_df(df_test, params['predictors']) # impute nans so that Nans do not propagate, when shifting with offsets.
 df_in = pd.concat((df_test, ), axis=0)
-#df_in = df_in[df_in.expocode.isin(expocodes)]
-pad = 64
+#df_in = df_in[df_in.expocode.isin(expocodes)] # select a subset of expocodes for faster processing
+pad = 64 # pad size
 ds = denoise_df_2(df_in, model_info, n_recs=n_recs, jump=None, n=pad)
 ds.set_index('index', inplace=True)
+
+# Finally rescale and add atmospheric co2 trend
 xco2_ix = params['predictors'].index('xco2') + 1
 ds.loc[:, rec_cols] = rescale(ds.loc[:, rec_cols].values.reshape(-1, 1), params, params['mode']).reshape(-1, n_recs)
 ds.loc[:, rec_cols] += (ds['xco2'].values[:, None] * params['train_stds'][xco2_ix] + params['train_means'][xco2_ix])
-#print(ds.head())
-
 
 df_in = df_in.reset_index()
 print(f"shape df_in: {df_in.shape}")
 def trim(df):
     len_expocode = len(df_in[df_in.expocode==df.expocode.iloc[0]])
-    return df.sort_values(by='window_id').iloc[pad:pad+len_expocode]
+    return df.sort_values(by='window_id').iloc[pad:pad+len_expocode] # remove padding
     
 ds = ds.groupby("expocode").apply(trim)
 print(f"shape ds: {ds.shape}")
-ds.to_parquet(f'{save_path}test_imputed.pq')
+ds.to_parquet(f'{save_path}selection_imputed.pq')
